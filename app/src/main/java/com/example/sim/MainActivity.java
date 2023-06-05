f9a4428c211e27cf1cc002b4cc7c271b981e546e
chaoscontrol01 <gianluca.diabl@gmail.com>
2023-06-05 16:27:45 +0200

    Added JavaDoc comments to MainActivity.java

--- app/src/main/java/com/example/sim/MainActivity.java
+++ app/src/main/java/com/example/sim/MainActivity.java
@@ -18,6 +18,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+/**
+ * The main activity of the Smart Inventory Manager app.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
@@ -35,6 +38,13 @@ public class MainActivity extends AppCompatActivity {
     // while using app (set false when app was closed)
     private static final String KEY_STAY_LOGGED_IN = "momentLoggedIn";
 
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
         CreateExtendedFAB();
     }
 
+    /**
+     * Initializes the extended floating action button and its sub-components.
+     */
     public void CreateExtendedFAB(){
         mAddFab = findViewById(R.id.add_fab);
         mAddListFab = findViewById(R.id.add_list_fab);
@@ -99,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-        // below is the sample action to handle add product FAB. Here it shows simple Toast msg. The Toast will be shown only when they are visible and only when user clicks on them
+        // below is the sample action to handle add product FAB. It initiates the loadProductActivity Method.
         mAddProductFab.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -107,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
                         loadProductActivity();
                     }
                 });
-        // below is the sample action to handle add list FAB. Here it shows simple Toast msg The Toast will be shown only when they are visible and only when user clicks on them
+        // below is the sample action to handle add list FAB. It initiates the loadListActivity Method.
         mAddListFab.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -117,46 +130,73 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    //@Override
-    //protected void onDestroy(){
-    //    super.onDestroy();
-    //    preferenceManager.setMomentLoggedIn(false);
-    //}
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        preferenceManager.setMomentLoggedIn(false);
+    }
 
-    // Methode für den Aufruf der LoginActivity
+    /**
+     * Loads the login activity.
+     */
     public void loadLoginActivity(){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         this.finish();
     }
-    // Methode für den Aufruf der ProductActivity
+
+    /**
+     * Loads the product activity.
+     */
     public void loadProductActivity(){
         Intent intent = new Intent(this, ProductActivity.class);
         startActivity(intent);
         this.finish();
     }
 
-    // Methode für den Aufruf der ListActivity
+    /**
+     * Loads the list activity.
+     */
     public void loadListActivity(){
         Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
         this.finish();
     }
 
-    // Methode für den Aufruf der PersonalActivity
+    /**
+     * Loads the personal activity.
+     */
     public void loadPersonalActivity(){
         Intent intent = new Intent(this, PersonalActivity.class);
         startActivity(intent);
         this.finish();
     }
 
+    /**
+     * Inflate the menu; this adds items to the action bar if it is present.
+     *
+     * @param menu The options menu in which you place your items.
+     *
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.profile_menu_nav, menu);
         return true;
     }
 
+    /**
+     * This is the so to say "onClick" Method for the MenuItem action_profile button. <br>
+     *
+     * When Clicked: <br>
+     * <p>
+     *     If the user is NOT logged in, the Login Activity will be called. <br>
+     *     If the user IS logged in, the Profile Activity will be called.
+     * </p>
+     *
+     * @param item The menu item that was selected.
+     *
+     */
+    @Override
     public boolean onOptionsItemSelected(MenuItem item){
         if (item.getItemId() == R.id.action_profile) {
            if(preferenceManager.isLoggedIn() == false && preferenceManager.isMomentLoggedIn() == false){
