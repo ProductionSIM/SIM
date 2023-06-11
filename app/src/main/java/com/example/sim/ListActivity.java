af017f72c87aaf8ddd65b48379b71e17d01b7c0e
chaoscontrol01 <gianluca.diabl@gmail.com>
2023-06-11 22:34:29 +0200

    Added DatePicker Button into activity_list.xml and activity_product.xml and tweaked Layout Added required code into ListActivity.java and ProductActivity.java

--- app/src/main/java/com/example/sim/ListActivity.java
+++ app/src/main/java/com/example/sim/ListActivity.java
@@ -1,5 +1,6 @@
 package com.example.sim;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,10 +8,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * The ListActivity class represents the activity for creating a new list.
@@ -19,7 +24,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ListActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editListName, editCreationDate, editStorageLocation;
-    Button btnCreateList;
+    Button btnCreateList, dateButton;
+
+    // Get the current date
+    Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
     final String databaseName = "/data/data/com.example.sim/databases/SIM.db";
 
@@ -36,6 +47,25 @@ public class ListActivity extends AppCompatActivity implements View.OnClickListe
 
         btnCreateList = (Button) findViewById(R.id.btnCreateList);
         btnCreateList.setOnClickListener(this);
+        dateButton = (Button) findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //     Get the current selected date from the spinner
+                // Create a DatePickerDialog to choose a new date
+                // Set initial date values
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ListActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Update the selected date EditText with the chosen date
+                        String formattedDate = String.format(Locale.getDefault(), "%02d.%02d.%04d", dayOfMonth, month + 1, year);
+                        editCreationDate.setText(formattedDate);
+                    }
+                }, year, month, dayOfMonth);
+
+                datePickerDialog.show();
+            }
+        });
     }
 
     /**
--- app/src/main/java/com/example/sim/ProductActivity.java
+++ app/src/main/java/com/example/sim/ProductActivity.java
@@ -1,5 +1,6 @@
 package com.example.sim;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -7,10 +8,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 /**
  * The ProductActivity class represents the activity for creating a new product.
@@ -19,7 +24,13 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ProductActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText editBrand, editProductTitle, editExpireDate, editPieceNumber;
-    Button btnCreateProduct;
+    Button btnCreateProduct, dateButton;
+
+    // Get the current date
+    Calendar calendar = Calendar.getInstance();
+    int year = calendar.get(Calendar.YEAR);
+    int month = calendar.get(Calendar.MONTH);
+    int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
 
     final String databaseName = "/data/data/com.example.sim/databases/SIM.db";
 
@@ -37,6 +48,25 @@ public class ProductActivity extends AppCompatActivity implements View.OnClickLi
 
         btnCreateProduct = (Button) findViewById(R.id.btnCreateProduct);
         btnCreateProduct.setOnClickListener(this);
+        dateButton = (Button) findViewById(R.id.dateButton);
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //     Get the current selected date from the spinner
+                // Create a DatePickerDialog to choose a new date
+                // Set initial date values
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ProductActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // Update the selected date EditText with the chosen date
+                        String formattedDate = String.format(Locale.getDefault(), "%02d.%02d.%04d", dayOfMonth, month + 1, year);
+                        editExpireDate.setText(formattedDate);
+                    }
+                }, year, month, dayOfMonth);
+
+                datePickerDialog.show();
+            }
+        });
     }
 
     /**
